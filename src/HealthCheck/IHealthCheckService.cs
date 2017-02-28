d6e30533720872168da3398f0d0596b044d8e216
Brad Wilson <dotnetguy@gmail.com>
2017-02-27 17:40:06 -0800

    Port code from https://github.com/aspnet/asplabs

---
+++ src/HealthCheck/IHealthCheckService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HealthChecks
+{
+    public interface IHealthCheckService
+    {
+        Task<bool> CheckHealthAsync();
+
+        HealthCheckResults CheckResults { get; set; }
+    }
+}
