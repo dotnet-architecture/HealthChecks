945adb64d5f24572c01677d2b3fd9acbdfe1bcd2
Brad Wilson <dotnetguy@gmail.com>
2017-03-18 13:13:17 -0700

    Add copyright headers to source files

--- samples/SampleHealthChecker/Controllers/HomeController.cs
+++ samples/SampleHealthChecker/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.HealthChecks;
 using Newtonsoft.Json;
--- test/Microsoft.Extensions.HealthChecks.Test/Internal/UrlCheckerTest.cs
+++ test/Microsoft.Extensions.HealthChecks.Test/Internal/UrlCheckerTest.cs
@@ -1,4 +1,7 @@
-﻿using NSubstitute;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Net;
