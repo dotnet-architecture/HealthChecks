a2d903040fc9ef14b10f52282ae0db541f443794
Brad Wilson <dotnetguy@gmail.com>
2017-04-05 11:45:52 -0700

    Missing copyright headers

--- src/Microsoft.AspNet.HealthChecks/GlobalHealthChecks.cs
+++ src/Microsoft.AspNet.HealthChecks/GlobalHealthChecks.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using Microsoft.Extensions.Logging;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 
 namespace Microsoft.Extensions.HealthChecks  // Put this in Extensions so you also have access to all the helper methods
 {
